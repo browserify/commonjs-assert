00dc6d5e635f0108f623c2dc884e837b9240366e
Luke Childs <lukechilds123@gmail.com>
2019-04-17 18:30:34 +0700

    Test userland assert and Node.js core assert

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 const test = require('tape');
 const glob = require('glob');
 const path = require('path');
+const proxyquire = require('proxyquire');
+
+const assert = require(process.env.TEST_NATIVE === 'true' ? 'assert' : '.');
 
 const testPaths = glob.sync('test/**/test-assert*.js');
 
 testPaths.forEach(testPath => {
   test(testPath, t => {
     t.doesNotThrow(() => {
-      require(path.resolve(__dirname, testPath));
+      proxyquire(path.resolve(__dirname, testPath), {assert});
     });
     t.end();
   });
