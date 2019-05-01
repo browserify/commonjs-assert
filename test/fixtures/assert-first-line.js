1ad38b9a593cd3de4e0f54de6852d9f689e1577a
Luke Childs <lukechilds123@gmail.com>
2019-05-01 14:20:13 +0700

    Remove first line tests

--- test/fixtures/assert-first-line.js
+++
@@ -1,2 +0,0 @@
-'use strict'; const ässört = require('../assert-loader'); ässört(true); ässört.ok(''); ässört(null);
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa(false);
