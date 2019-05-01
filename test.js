769a40048b84b2a001bb0a1740e46d67d87e8ad0
Luke Childs <lukechilds123@gmail.com>
2019-05-01 14:31:43 +0700

    Create require wrapper so browserify can resolve test files

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 const test = require('tape');
 
 const testPaths = [
-  './test/parallel/test-assert-async.js',
-  './test/parallel/test-assert-checktag.js',
-  './test/parallel/test-assert-deep.js',
-  './test/parallel/test-assert-fail-deprecation.js',
-  './test/parallel/test-assert-fail.js',
-  './test/parallel/test-assert-if-error.js',
-  './test/parallel/test-assert-typedarray-deepequal.js',
-  './test/parallel/test-assert.js',
-  './test/pseudo-tty/test-assert-colors.js',
-  './test/pseudo-tty/test-assert-no-color.js',
-  './test/pseudo-tty/test-assert-position-indicator.js'
+  ['test-assert-async.js', () => require('./test/parallel/test-assert-async.js')],
+  ['test-assert-checktag.js', () => require('./test/parallel/test-assert-checktag.js')],
+  ['test-assert-deep.js', () => require('./test/parallel/test-assert-deep.js')],
+  ['test-assert-fail-deprecation.js', () => require('./test/parallel/test-assert-fail-deprecation.js')],
+  ['test-assert-fail.js', () => require('./test/parallel/test-assert-fail.js')],
+  ['test-assert-if-error.js', () => require('./test/parallel/test-assert-if-error.js')],
+  ['test-assert-typedarray-deepequal.js', () => require('./test/parallel/test-assert-typedarray-deepequal.js')],
+  ['test-assert.js', () => require('./test/parallel/test-assert.js')],
+  ['test-assert-colors.js', () => require('./test/pseudo-tty/test-assert-colors.js')],
+  ['test-assert-no-color.js', () => require('./test/pseudo-tty/test-assert-no-color.js')],
+  ['test-assert-position-indicator.js', () => require('./test/pseudo-tty/test-assert-position-indicator.js')]
 ];
 
-testPaths.forEach(testPath => {
-  test(testPath, t => {
+testPaths.forEach(([testName, requireTest]) => {
+  test(testName, t => {
     t.plan(2)
     let result;
     t.doesNotThrow(() => {
-      result = require(testPath);
+      result = requireTest();
     });
     Promise.resolve(result)
       .then(() => false)
